ba3643610f7fde592f1574e06a7902b329b5427a
Patrik <patriklytschmann@web.de>
2024-04-07 21:11:21 +0900

    First Implementation of search_boolean_model

--- src/information_retrieval/boolean_model.py
+++ src/information_retrieval/boolean_model.py
@@ -6,15 +6,16 @@ from prisma import Prisma
 from db.posts import get_all_posts, create_post
 from information_retrieval.linked_list import LinkedList
 
-inverted_index = {} # Store Posting Lists in a dictionary with the word as the key and the value as a list of post_ids
-term_frequency = {} # Store the term frequency of each word
+_inverted_index = {} # Store Posting Lists in a dictionary with the word as the key and the value as a list of post_ids
+_term_frequency = {} # Store the term frequency of each word
+_all_doc_ids = {} # Store all the document ids
 
 async def build_boolean_model():
     prisma_client = Prisma()
     await prisma_client.connect()
     
-    global inverted_index
-    global term_frequency 
+    global _inverted_index
+    global _term_frequency 
     
     # Get all posts content
     posts = await get_all_posts(prisma_client).values_list("id", "content")
@@ -23,31 +24,55 @@ async def build_boolean_model():
     for post in posts:
         words = post[1].split()
         for word in words:
-            if word in inverted_index:
-                inverted_index[word].insertSorted(post[0])
-                term_frequency[word] += 1
+            if word in _inverted_index:
+                _inverted_index[word].insertSorted(post[0])
+                _term_frequency[word] += 1
             else:
-                inverted_index[word] = LinkedList(post[0])
-                term_frequency[word] = 1
+                _inverted_index[word] = LinkedList(post[0])
+                _term_frequency[word] = 1
+                _all_doc_ids.add(post[0])
     
     await prisma_client.disconnect()
     
 def search_boolean_model(query):
+    id_set = _all_doc_ids.copy()
     
+    """
     # For testing purposes, should be removed later
     words = [entry[1] for entry in query]
-    term_frequency[words[0]] = 10 
-    term_frequency[words[1]] = 30
-    term_frequency[words[2]] = 2
-    term_frequency[words[3]] = 5
+    _term_frequency[words[0]] = 10 
+    _term_frequency[words[1]] = 30
+    _term_frequency[words[2]] = 2
+    _term_frequency[words[3]] = 5
+    """
     
     # First search tokens by frequency
-    sorted_query = sorted(query, key=lambda word: term_frequency[word[1]])
+    sorted_query = sorted(query, key=lambda word: _term_frequency[word[1]])
     
     for entry in sorted_query:
-        # ToDo implement search logic here
-        # Look through the inverted index and find the post_ids that contain the word
-        # store the post_ids in a list
-        # Then perform the boolean operation on the list
+        # Call different functions based on the operator
+        if entry[0] == "AND":
+            id_set = _and_processing(entry[1], id_set)
+        elif entry[0] == "OR": 
+            id_set = _or_processing(entry[1], id_set)
+        elif entry[0] == "NOT":
+            id_set = _not_processing(entry[1], id_set)
+
         print(entry[0], entry[1])
-    return sorted_query
+        
+    return id_set
+
+def _and_processing(word, id_set):
+    ids_of_index = _inverted_index[word]
+    id_set = id_set.intersection(ids_of_index)
+    return id_set
+
+def _or_processing(word, id_set):
+    ids_of_index = _inverted_index[word]
+    id_set = id_set.union(ids_of_index)
+    return id_set
+
+def _not_processing(word, id_set):
+    ids_of_index = _inverted_index[word]
+    id_set = id_set.difference(ids_of_index)
+    return id_set
