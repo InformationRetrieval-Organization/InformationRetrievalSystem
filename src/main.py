b521481ca6c76daff9df3162c04d092bb2ed7827
Tony <tonymeissner70@gmail.com>
2024-04-08 19:41:59 +0900

    Add database initialization in main function

--- src/main.py
+++ src/main.py
@@ -5,12 +5,16 @@ from api.boolean_api import boolean_search_blueprint
 from data_crawlers.nyt_data_crawler import crawl_nyt_data
 from information_retrieval.vector_space_model import build_vector_space_model
 from information_retrieval.boolean_model import build_boolean_model
+from db.helper import init_database
 
 app = Flask(__name__)
 app.register_blueprint(vector_space_search_blueprint)
 app.register_blueprint(boolean_search_blueprint)
 
 async def main():
+    # init the database
+    await init_database()
+
     # Crawl data from New York Times and store in database
     await crawl_nyt_data()
 
