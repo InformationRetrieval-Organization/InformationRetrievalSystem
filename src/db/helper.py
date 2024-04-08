c4c89771620bd9c12f9d57b34ee8e6f032b8b6a3
Tony <tonymeissner70@gmail.com>
2024-04-09 00:14:01 +0900

    Add delete_processed_posts() function and insert_posts() function

--- src/db/helper.py
+++ src/db/helper.py
@@ -1,14 +1,21 @@
 import pandas as pd
 from db.posts import create_post, delete_posts
+from db.processed_posts import delete_processed_posts
 import os
 from datetime import datetime
 
 async def init_database():
-    # Delete existing posts
     await delete_posts()
+    await delete_processed_posts()
+    await insert_posts()
 
+async def insert_posts():
     cwd = os.getcwd()
     file_path = os.path.join(cwd, 'files', 'Post.csv')
+
+    # check if the file exists
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
 
     df = pd.read_csv(file_path)
 
