bcce2dc745892d75b410b592d454ce7a47e8374f
Tony <tonymeissner70@gmail.com>
2024-04-08 20:36:02 +0900

    Refactor NYT data crawler to use NYT API directly

--- src/data_crawlers/nyt_data_crawler.py
+++ src/data_crawlers/nyt_data_crawler.py
@@ -1,29 +1,59 @@
 import os
-from pynytimes import NYTAPI
 import asyncio
 from datetime import datetime
 from db.posts import get_all_posts, create_post
+import requests
+import json
+
 
 async def crawl_nyt_data() -> None:
     nyt_api_key = os.getenv("NYT_API_KEY")
-    nyt = NYTAPI(nyt_api_key, parse_dates=True)
-    begin_date = datetime.strptime("2024-03-01", "%Y-%m-%d").date()
-    end_date = datetime.strptime("2024-04-01", "%Y-%m-%d").date()
+    begin_date = datetime.strptime("20240301", "%Y%m%d").date()
+    end_date = datetime.strptime("20240401", "%Y%m%d").date()
 
-    # ['abstract', 'web_url', 'snippet', 'lead_paragraph', 'print_section', 'print_page', 'source', 'multimedia', 'headline', 'keywords', 'pub_date', 'document_type', 'news_desk', 'section_name', 'subsection_name', 'byline', 'type_of_material', '_id', 'word_count', 'uri']
-    articles = nyt.article_search(query="korea", 
-                                dates={"begin": begin_date, "end": end_date}, 
-                                results=10)
+    # because there is a limit of 10 articles per page, we need to loop through all pages
+    page = 1
+    while True:
+        articles = get_articles(query="korea", 
+                                begin_date=begin_date, 
+                                end_date=end_date, 
+                                api_key=nyt_api_key, 
+                                page=page)
+        if not articles:
+            break
 
-    for article in articles:
-        await create_post(
-            title=article["headline"]["main"], 
-            content=article["abstract"], 
-            published_on=article["pub_date"], 
-            link=article["web_url"]
-        )
+        for article in articles:
+            await create_post(
+                title=article["headline"]["main"], 
+                content=article["abstract"], 
+                published_on=datetime.fromisoformat(article["pub_date"]),
+                link=article["web_url"]
+            )
 
+        page += 1
+
+    """
     # print all posts
-    # posts = await get_all_posts()
-    # for post in posts:
-        #print(post)
+    posts = await get_all_posts()
+    for post in posts:
+        print(post) 
+    """
+
+
+def get_articles(query, begin_date, end_date, api_key, page=1):
+    url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+    params = {
+        "q": query,
+        "begin_date": begin_date,
+        "end_date": end_date,
+        "api-key": api_key,
+        "page": page
+    }
+
+    # limit of 1 request per minute and 500 per day
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()["response"]["docs"]
+    else:
+        return None
