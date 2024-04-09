c813b61c7b987a5e32d5fce43a6c4cd68e0a16ca
LukasWelker <lukas.welker@tecalliance.net>
2024-04-09 17:58:40 +0900

    added global variable for weight_matrix

--- src/information_retrieval/globals.py
+++ src/information_retrieval/globals.py
@@ -2,7 +2,10 @@ def init():
     global _inverted_index
     global _term_frequency
     global _all_doc_ids
+    global _term_document_weight_matrix
 
     _inverted_index = {} # Store Posting Lists in a dictionary with the word as the key and the value as a list of post_ids
     _term_frequency = {} # Store the term frequency of each word
     _all_doc_ids = set() # Store all the document ids
+    _term_document_weight_matrix = []
+    
