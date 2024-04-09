2f4d14fe758eef7d5768f7ba58b7f50a70f6c6f9
LukasWelker <lukas.welker@tecalliance.net>
2024-04-09 23:22:53 +0900

    added basic logic for search vector space

--- src/information_retrieval/vector_space_model.py
+++ src/information_retrieval/vector_space_model.py
@@ -4,11 +4,42 @@ import math
 from db.processed_posts import get_all_processed_posts
 import information_retrieval.globals
 import information_retrieval.linked_list
+import numpy as np
 
 
-def search_vector_space(query):
+async def search_vector_space(query):
+    posts = await get_all_processed_posts()
+    posts = [(post.id, post.content) for post in posts]
+
+    # Step 2: Calculate the document frequency (DF) for each term
+    total_documents = len(posts)
+    inverse_document_frequency = {}
+    query_weight_matrix = []
+
+    for term in query:
+        df = information_retrieval.globals._inverted_index[term].length()
+        # Step 3: Calculate the inverse document frequency (IDF) for each term
+        inverse_document_frequency[term] = compute_inverse_document_frequency(total_documents, df)
+ 
+    for post in posts:
+        #every post has its own vector creating this vector
+        tfidf_vector = [compute_tf_idf_weighting(compute_sublinear_tf_scaling(post[1].count(term)), inverse_document_frequency[term]) for term in query]
+        query_weight_matrix = tfidf_vector
+
+    doc_cosine_similiarity_map = {}
+
+    for doc_id, vector in information_retrieval.globals._document_id_vector_map.items():
+        dot_product = np.dot(query_weight_matrix, vector)
+        magnitude_query = np.linalg.norm(query_weight_matrix)
+        magnitude_entry = np.linalg.norm(vector)
+        cosine_similarity = dot_product / (magnitude_query * magnitude_entry)
+        doc_cosine_similiarity_map[doc_id] = cosine_similarity
+    sorted_docs = sorted(doc_cosine_similiarity_map.items(), key=lambda x: x[1], reverse=True)
+
+    # Extract the sorted document IDs into a list
+    sorted_doc_ids = [doc_id for doc_id, _ in sorted_docs]
     
-    return query
+    return sorted_doc_ids
 
 async def build_vector_space_model(vocabulary):
     """
@@ -20,21 +51,18 @@ async def build_vector_space_model(vocabulary):
     posts = [(post.id, post.content) for post in posts]
 
     # Step 2: Calculate the document frequency (DF) for each term
-    document_frequency = {}
     total_documents = len(posts)
     inverse_document_frequency = {}
     for term in vocabulary:
         df = information_retrieval.globals._inverted_index[term].length()
         # Step 3: Calculate the inverse document frequency (IDF) for each term
-        document_frequency[term] = df
         inverse_document_frequency[term] = compute_inverse_document_frequency(total_documents, df)
- 
+
     for post in posts:
         #every post has its own vector creating this vector
         tfidf_vector = [compute_tf_idf_weighting(compute_sublinear_tf_scaling(post[1].count(term)), inverse_document_frequency[term]) for term in vocabulary]
         information_retrieval.globals._term_document_weight_matrix.append(tfidf_vector)
-
-    
+        information_retrieval.globals._document_id_vector_map[post[0]] = tfidf_vector 
     print("Vector Space Model Built")
     return None
 
