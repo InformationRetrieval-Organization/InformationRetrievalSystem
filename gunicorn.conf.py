33c120bd6c48b5a1a3e6926f51699856bca524d1
Tony <tonymeissner70@gmail.com>
2024-04-19 13:07:44 +0900

    Refactor gunicorn.conf.py to use more workers for the server

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,6 @@ import multiprocessing
 
 pythonpath = 'src'
 bind = '0.0.0.0:3100'
-workers = 1
-#workers = multiprocessing.cpu_count() * 2 + 1 # maximum workers for the server
+workers = multiprocessing.cpu_count() # * 2 + 1
 worker_class = 'uvicorn.workers.UvicornWorker'
 timeout = 600 # 600s startup time for database actions
